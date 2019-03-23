4afe82bb5620811017bc9aa6bbdf19068645b25e
spasoje <spasojepetronijevic@gmail.com>
2019-03-23 09:29:26 +0100

    Adding loadings in reducer. Now it is possible to track loading status for every action.

--- eventify-frontend/src/redux/event/reducer.js
+++ eventify-frontend/src/redux/event/reducer.js
@@ -8,25 +8,38 @@ const initialState = {
     eventsFilter:{
         priceFrom:null
     },
-    loading:null,
+    loadings:null,
     eventById:null
 }
+
+const storeLoadingByAction = (loading, action) => {
+    let calls = action.type;
+    debugger;
+    if (action.type.endsWith("SUCCESS")) {
+        calls = action.type.substring(0, action.type.length - 8);
+    }
+    if (action.type.endsWith("FAIL")) {
+        calls = action.type.substring(0, action.type.length - 5);
+    }
+    return {[calls]:loading};
+
+};
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
 
         case types.GET_EVENTS_BY_FILTER_SUCCESS:
-            return {...state, events:action.payload.data._embedded.eventResourceList, loading:false};
+            return {...state, events:action.payload.data._embedded.eventResourceList, loadings:storeLoadingByAction(true, action)};
         case types.GET_EVENTS_BY_FILTER:
-            return {...state, loading:true};
+            return {...state, loadings:storeLoadingByAction(true, action)};
         case types.GET_EVENTS_BY_FILTER_FAIL:
-            return {...state, loading:false};
+            return {...state, loadings:false};
         case types.GET_EVENT_BY_ID_SUCCESS:
-            return {...state, eventById:action.payload.data._embedded, loading:true};
+            return {...state, eventById:action.payload.data._embedded, loadings:true};
         case types.GET_EVENT_BY_ID:
-            return {...state, loading:false};
+            return {...state, loadings:false};
         case types.GET_EVENT_BY_ID_FAIL:
-            return {...state, loading:false};
+            return {...state, loadings:false};
         case types.CHANGE_FILTER_SUCCESS:
             const eventsFilter = action.eventsFilter;
             return {...state,eventsFilter:eventsFilter};
