9d92126ee9270b33e5ab5e78fd52115473e5d220
spasoje <spasojepetronijevic@gmail.com>
2019-05-18 10:25:34 +0200

    event controller integration test added

--- eventify-backend/event/src/test/java/com/eventify/events/api/rest/EventControllerIntegrationTest.java
+++ eventify-backend/event/src/test/java/com/eventify/events/api/rest/EventControllerIntegrationTest.java
@@ -3,6 +3,7 @@ package com.eventify.events.api.rest;
 import com.eventify.events.domain.Event;
 import com.eventify.events.infrastructure.EventRepository;
 import com.eventify.events.infrastructure.KafkaEventProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
 import org.junit.After;
@@ -17,13 +18,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -83,7 +85,57 @@ public class EventControllerIntegrationTest {
 
         //then
         Event event = eventRepository.findByEventName("name insert");
-        Assertions.assertThat(event).isNotNull();
-        Assertions.assertThat(event.getEventName()).isEqualTo("name insert");
+        assertThat(event).isNotNull();
+        assertThat(event.getEventName()).isEqualTo("name insert");
+        assertThat(event.getDescription()).isEqualTo("desc insert");
+    }
+
+    @Test
+    public void deleteEventTest() throws Exception {
+        //given
+        Event event = new Event();
+        event.setDescription("desc");
+        event.setEventDateTime(LocalDateTime.now());
+        event.setEventName("event name");
+        event.setEventType("theater");
+        eventRepository.save(event);
+        doNothing().when(kafkaEventProducer).send(any());
+
+        //when
+        this.mvc.perform(delete("/events/{id}",event.getEventId())
+                .contentType(APPLICATION_JSON)
+        );
+
+        //then
+        assertThat(eventRepository.findAll()).hasSize(0);
+
+    }
+
+    @Test
+    public void updateEventTest() throws Exception {
+        //given
+        Event event = new Event();
+        event.setDescription("desc");
+        event.setEventDateTime(LocalDateTime.now());
+        event.setEventName("event name");
+        event.setEventType("theater");
+        eventRepository.save(event);
+        UpdateEventRequest updateEventRequest = new UpdateEventRequest();
+        String new_desc = "new desc";
+        updateEventRequest.setDescription(new_desc);
+        LocalDateTime newTime = LocalDateTime.now().minusDays(2);
+        updateEventRequest.setEventDateTime(newTime);
+        doNothing().when(kafkaEventProducer).send(any());
+
+        //when
+        this.mvc.perform(put("/events/{id}", event.getEventId())
+                .contentType(APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updateEventRequest)));
+
+        Event updatedEvent = eventRepository.findById(event.getEventId()).orElse(null);
+        assertThat(updatedEvent).isNotNull();
+        assertThat(updatedEvent.getDescription()).isEqualTo(new_desc);
+        assertThat(updatedEvent.getEventDateTime()).isEqualTo(newTime);
+        assertThat(updatedEvent.getEventName()).isEqualTo(event.getEventName());
     }
 }
