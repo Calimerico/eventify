57e895ca6f4bbbac850e61c528b7071f8cc21274
spasoje <spasojepetronijevic@gmail.com>
2019-05-27 21:05:19 +0200

    login path fix

--- eventify-backend/eventify-shared/src/main/java/com/eventify/shared/demo/JwtConfig.java
+++ eventify-backend/eventify-shared/src/main/java/com/eventify/shared/demo/JwtConfig.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JwtConfig {
-    @Value("${security.jwt.uri:/**}")
+    @Value("${security.jwt.uri:/login/**}")
     private String Uri;
 
     @Value("${security.jwt.header:Authorization}")
