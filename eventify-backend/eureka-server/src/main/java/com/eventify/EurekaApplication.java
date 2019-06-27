8fcdfb9866a20cc24055a1bf42277c8f776725e9
Calimerico <2sk!E69x-Fd>
2019-06-27 23:39:43 +0200

    eureka Cannot execute request on any known server fix

--- eventify-backend/eureka-server/src/main/java/com/eventify/EurekaApplication.java
+++ eventify-backend/eureka-server/src/main/java/com/eventify/EurekaApplication.java
@@ -3,6 +3,14 @@ package com.eventify;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -10,5 +18,20 @@ public class EurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
+	}
+	//TODO I had 401 problem so I added this config
+	//https://github.com/spring-cloud/spring-cloud-netflix/issues/2754#issuecomment-372808529
+	@EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+				.csrf().disable()
+					.authorizeRequests()
+					.antMatchers(HttpMethod.GET, "/**").permitAll()
+					.antMatchers(HttpMethod.POST, "/**").permitAll()
+					.antMatchers(HttpMethod.PUT, "/**").permitAll()
+					.antMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+		}
 	}
 }
