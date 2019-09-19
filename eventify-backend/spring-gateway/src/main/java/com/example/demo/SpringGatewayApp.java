1a9befa1727a84084d3bd76d3b3b29ca7efa1a10
Calimerico <2sk!E69x-Fd>
2019-09-19 14:21:14 +0200

    spring gateway

---
+++ eventify-backend/spring-gateway/src/main/java/com/example/demo/SpringGatewayApp.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringGatewayApp {
+
+	@Value("${userService.url}")
+	private String userServiceUrl;
+	@Value("${eventService.url}")
+	private String eventServiceUrl;
+	@Value("${webScraperService.url}")
+	private String webScraperServiceUrl;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGatewayApp.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/event/**")
+						.filters(f -> f.rewritePath("/event/", "/"))
+						.uri(eventServiceUrl)
+						.id("event-service"))
+				.route(p -> p
+						.path("/user/**")
+						.filters(f -> f.rewritePath("/user/", "/"))
+						.uri(userServiceUrl)
+						.id("user-service"))
+				.route(p -> p
+						.path("/scraper/**")
+						.filters(f -> f.rewritePath("/scraper/", "/"))
+						.uri(webScraperServiceUrl)
+						.id("web-scraper"))
+				.build();
+	}
+
+}
