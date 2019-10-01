fb8622fb376f8f08222e7b6071836dd78e4a0f84
Calimerico <2sk!E69x-Fd>
2019-10-01 11:30:33 +0200

    exceptions handled better with @ExceptionHandler

--- eventify-backend/eventify-shared/src/main/java/com/eventify/shared/demo/ExceptionsHandler.java
+++ eventify-backend/eventify-shared/src/main/java/com/eventify/shared/demo/ExceptionsHandler.java
@@ -1,12 +1,18 @@
 package com.eventify.shared.demo;
 
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Collections.singletonList;
 
 /**
  * Created by spasoje on 09-Feb-19.
@@ -14,7 +20,28 @@ import org.springframework.web.context.request.WebRequest;
 @ControllerAdvice
 public class ExceptionsHandler {
     @ExceptionHandler(value = {DataIntegrityViolationException.class})
-    protected ResponseEntity<DataIntegrityViolationException> handleConflict(DataIntegrityViolationException ex, WebRequest request) {
-        return new ResponseEntity<>(ex,HttpStatus.CONFLICT);
+    protected ResponseEntity<Map<String, Object>> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("errors", singletonList(ex.getMessage()));
+        //todo we are exposing part of the exception like this, we should remap it to error message somehow
+        return new ResponseEntity<>(body,HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    protected ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+
+        //Get all errors
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + " " + fieldError.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 }
